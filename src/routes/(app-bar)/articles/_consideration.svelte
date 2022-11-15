d8bb39f9f44dd3170a33fe7940c5ef5fcaa0a94d
wappon-28-dev <wappon_28_dev@ruru.be>
2022-11-15 18:22:27 +0900

    :sparkles: 研究方法と結果と考察を追加したよ

--- src/routes/(app-bar)/articles/_consideration.svelte
+++ src/routes/(app-bar)/articles/_consideration.svelte
@@ -7,28 +7,10 @@
                 要求された機能は全て実装することができた.　ただ,
                 Qiitaなどのユーザー投稿型のサイトと機能を比べると,
                 まだ実装や作り込みの足りない部分があると考えた.
-                　今回制作した“スキールネット” と ユーザー投稿型サイト Qiita
-                のアカウント登録画面の比較を図６に示す：
             </p>
-        </div>
-        <div class="item image">
-            <img
-                src="https://uploads-ssl.webflow.com/603c87adb15be3cb0b3ed9b5/60f95161b382b3c00f3b3057_80_cat_box_ol.png"
-                alt=""
-            />
-        </div>
-    </div>
-    <div class="grid">
-        <div class="item image">
-            <img
-                src="https://uploads-ssl.webflow.com/603c87adb15be3cb0b3ed9b5/60f95161b382b3c00f3b3057_80_cat_box_ol.png"
-                alt=""
-            />
-        </div>
-        <div class="item article">
             <p>
-                &nbsp;&nbsp;&nbsp;図６から, Qiitaのアカウント登録画面には,
-                ユーザー名, メールアドレスとパスワードの 一般的な登録以外にも,
+                &nbsp;&nbsp;&nbsp;Qiitaのアカウント登録画面には, ユーザー名,
+                メールアドレスとパスワードの 一般的な登録以外にも,
                 GitHubやTwitterなどの多くのソーシャルサービスの紐づけに対応している.　一方
                 “スキールネット”には,
                 Googleアカウントのみの連携しか現状できない.　学校での生徒の利用に問題は無くとも,
@@ -42,6 +24,16 @@
                 reCAPTCHAをバンドルすることで,
                 ユーザーが人間であることを証明でき,
                 しっかりと対策されていると考えることができる.
+            </p>
+        </div>
+        <div class="item image">
+            <img
+                src="img/articles/09.png"
+                alt="今回制作した“スキールネット” と ユーザー投稿型サイトQiitaのアカウント登録画面の比較"
+            />
+            <p>
+                ↑ “スキールネット” と
+                ユーザー投稿型サイトQiitaのアカウント登録画面の比較
             </p>
         </div>
     </div>
